6457026fa5c19d6bbcb8a4276c0e7bb652d57c00
Kasper Lynderup Jensen <klj@it-minds.dk>
2021-04-12 19:26:30 +0200

    removed apt-get install

--- chart/scripts/test-ingres-route.sh
+++ chart/scripts/test-ingres-route.sh
@@ -1,6 +1,3 @@
-apt-get update > /dev/null 2>&1
-apt-get install -y curl > /dev/null 2>&1
-
 timeout 5m bash -c /scripts/check-for-ip.sh
 
 echo "$(kubectl get services ingress-mukube-ingress-loadbalancer -n ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')    test-app" >> /etc/hosts
